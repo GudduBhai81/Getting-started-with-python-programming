51fe1590a3303d40824fd81f4003a42eaf3244e4
GudduBhai81 <306botala@gmail.com>
2024-09-03 08:53:45 -0700

    Add files via upload

---
+++ print.py
@@ -0,0 +1,4 @@
+#printing statement about me
+print("my name is Abdullah Saqib")
+#performing and printing simple calculations
+print("the result of 1+2 = ", 1+2)
---
+++ variables.py
@@ -0,0 +1,6 @@
+x = 1
+y = "Guddu Gamerz 2"
+name = input("enter your name")
+print(" \n hello", " \n Welcome to my channel")
+print(x)
+print(y)
